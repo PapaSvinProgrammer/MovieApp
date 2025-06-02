567d1834ba895e8b9dee697a351c4c77f9c12a66
romanurajkin <UraykinRab@yandex.ru>
2025-06-02 14:21:43 +0300

    Added CustomSearchBar

--- app/src/main/java/com/example/movieapp/ui/widget/component/TextListLayout.kt
+++ app/src/main/java/com/example/movieapp/ui/widget/component/TextListLayout.kt
@@ -30,6 +30,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -108,14 +112,23 @@ private fun MainContent(
     result: List<Pair<String, Boolean>>,
     onClick: (Int) -> Unit
 ) {
+    var query by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(innerPadding)
     ) {
+        CustomSearchBar(
+            query = query,
+            onValueChange = { query = it }
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
         LazyColumn(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .haze(hazeState)
         ) {
             items(result.size) { index ->
